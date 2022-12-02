837790959189781e802b3f132061caa72acc8bcc
htmlcsjs <46023024+htmlcsjs@users.noreply.github.com>
2022-12-02 20:02:49 +0000

    Day 2: Rock Paper Scissors

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,10 @@ use std::{
     path::Path,
 };
 
-use crate::day_one::DayOne2022;
+use crate::{day_one::DayOne2022, day_two::DayTwo2022};
 
 mod day_one;
+mod day_two;
 
 trait Day {
     fn first_puzzle(&self, input: &mut BufReader<File>) -> String;
@@ -40,7 +41,7 @@ impl Day for Day0Year22 {
 }
 
 fn main() {
-    let days: Vec<&dyn Day> = vec![&DayOne2022];
+    let days: Vec<&dyn Day> = vec![&DayOne2022, &DayTwo2022];
     println!("Current puzzles:");
     for (i, day) in days.iter().enumerate() {
         println!("{}: Day {}, Year {}", i + 1, day.day(), day.year());
