c82142011db27c45f9bc7944d99194f2a270d6db
htmlcsjs <46023024+htmlcsjs@users.noreply.github.com>
2022-12-11 19:48:58 +0000

    Day 10: Cathode-Ray Tube

---
+++ aoc/src/day_ten.rs
@@ -0,0 +1,86 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+use regex::Regex;
+
+use crate::{AOCError, Day};
+
+pub struct DayTen2022;
+impl Day for DayTen2022 {
+    fn first_puzzle(&self, input: &mut BufReader<File>) -> Result<String, AOCError> {
+        let mut iter = input.lines().peekable();
+        let mut cycle: i64 = 0;
+        let mut wait = 0;
+        let mut instr = "".to_string();
+        let mut x: i64 = 1;
+        let addx = Regex::new("addx ([-\\d]+)")?;
+        let mut strengths: Vec<i64> = Vec::new();
+
+        while iter.peek().is_some() {
+            cycle += 1;
+            if cycle % 40 == 20 {
+                strengths.push(x * cycle);
+            }
+            if wait == 0 {
+                instr = iter.next().unwrap()?;
+            }
+            if addx.is_match(&instr) {
+                if wait == 0 {
+                    wait = 1;
+                } else if wait == 1 {
+                    x += addx.captures(&instr).expect("No captures in addx inst")[1].parse::<i64>()?;
+                    wait -= 1;
+                }
+            } else if instr == "noop" {
+                wait = 0;
+            }
+        }
+        Ok(strengths.iter().sum::<i64>().to_string())
+    }
+
+    fn second_puzzle(&self, input: &mut BufReader<File>) -> Result<String, AOCError> {
+        let mut iter = input.lines().peekable();
+        let mut cycle: i64 = 0;
+        let mut end_cycle = 0;
+        let mut x: i64 = 1;
+        let mut inc: i64 = 0;
+        let addx = Regex::new("addx ([-\\d]+)")?;
+        let mut rendered = "".to_string();
+
+        while iter.peek().is_some() {
+            cycle += 1;
+            let pixel = (cycle - 1) % 40;
+            if pixel == 0 && cycle != 1 {
+                rendered += "\n"
+            }
+            if ((pixel - 1)..=(pixel + 1)).contains(&x) {
+                rendered += "#";
+            } else {
+                rendered += " ";
+            }
+            if end_cycle == cycle {
+                x += inc;
+            }
+            if end_cycle < cycle {
+                let instr = iter.next().unwrap()?;
+
+                if addx.is_match(&instr) {
+                    inc = addx.captures(&instr).expect("No captures in addx inst")[1].parse::<i64>()?;
+                    end_cycle = cycle + 1;
+                }
+            }
+        }
+        println!("{}", rendered);
+        Ok("See above".to_string())
+    }
+
+    fn day(&self) -> i16 {
+        10
+    }
+
+    fn year(&self) -> i16 {
+        2022
+    }
+}
--- aoc/src/main.rs
+++ aoc/src/main.rs
@@ -2,7 +2,8 @@ use std::{
     fs::File,
     io::{self, BufReader, Seek},
     num::ParseIntError,
-    path::Path, time::Instant,
+    path::Path,
+    time::Instant,
 };
 
 use ::error_macro::ErrorWrapper;
@@ -10,7 +11,7 @@ use itertools::Itertools;
 
 use crate::{
     day_eight::DayEight2022, day_five::DayFive2022, day_four::DayFour2022, day_nine::DayNine2022, day_one::DayOne2022,
-    day_seven::DaySeven2022, day_six::DaySix2022, day_three::DayThree2022, day_two::DayTwo2022,
+    day_seven::DaySeven2022, day_six::DaySix2022, day_ten::DayTen2022, day_three::DayThree2022, day_two::DayTwo2022,
 };
 
 mod day_eight;
@@ -20,6 +21,7 @@ mod day_nine;
 mod day_one;
 mod day_seven;
 mod day_six;
+mod day_ten;
 mod day_three;
 mod day_two;
 mod error_macro;
@@ -51,9 +53,11 @@ fn main() {
         &DaySeven2022,
         &DayEight2022,
         &DayNine2022,
+        &DayTen2022,
     ];
-    let mut table = vec!["Day", "A Result", "B Result", "A Time", "B Time"].into_iter().map(|x| vec![x.to_string()]).collect_vec();
-    
+    let mut table =
+        vec!["Day", "A Result", "B Result", "A Time", "B Time"].into_iter().map(|x| vec![x.to_string()]).collect_vec();
+
     for day in days {
         table[0].push(day.day().to_string());
 
@@ -63,14 +67,14 @@ fn main() {
             Err(_) => {
                 table[1].push(format!("Couldnt read file {}", path.display()));
                 table[2].push("<-".to_string());
-                table[3].push("0 s".to_string());
-                table[4].push("0 s".to_string()); 
+                table[3].push("  0.000s".to_string());
+                table[4].push("  0.000s".to_string());
                 continue;
-            },
+            }
             Ok(file) => file,
         };
         let mut buf_reader = BufReader::new(file);
-        
+
         let p1start = Instant::now();
         match day.first_puzzle(&mut buf_reader) {
             Ok(a) => table[1].push(a),
@@ -79,7 +83,7 @@ fn main() {
         table[3].push(format!("{:>09.3?}", p1start.elapsed()));
         if let Err(e) = buf_reader.rewind() {
             table[2].push(format!("Error rewinding: {}", e));
-            table[4].push("0 s".to_string());
+            table[4].push("  0.000s".to_string());
         }
 
         let p2start = Instant::now();
@@ -90,12 +94,21 @@ fn main() {
         table[4].push(format!("{:>09.3?}", p2start.elapsed()));
     }
 
-    let widths = table.iter().map(|x| x.iter().max_by(|x,y| x.len().cmp(&y.len())).unwrap()).map(|x| x.len()).collect_vec();
+    let widths =
+        table.iter().map(|x| x.iter().max_by(|x, y| x.len().cmp(&y.len())).unwrap()).map(|x| x.len()).collect_vec();
     println!("{}", "╔═".to_string() + &widths.iter().map(|x| "═".repeat(*x)).join("═╦═") + "═╗");
-    println!("{}", "║ ".to_string() + &table.iter().enumerate().map(|(pos, x)| pad_string(&x[0], widths[pos])).join(" ║ ") + " ║");
+    println!(
+        "{}",
+        "║ ".to_string() + &table.iter().enumerate().map(|(pos, x)| pad_string(&x[0], widths[pos])).join(" ║ ") + " ║"
+    );
     println!("{}", "╠═".to_string() + &widths.iter().map(|x| "═".repeat(*x)).join("═╬═") + "═╣");
     for i in 1..table[0].len() {
-        println!("{}", "║ ".to_string() + &table.iter().enumerate().map(|(pos, x)| pad_string(&x[i], widths[pos])).join(" ║ ") + " ║");
+        println!(
+            "{}",
+            "║ ".to_string()
+                + &table.iter().enumerate().map(|(pos, x)| pad_string(&x[i], widths[pos])).join(" ║ ")
+                + " ║"
+        );
     }
     println!("{}", "╚═".to_string() + &widths.iter().map(|x| "═".repeat(*x)).join("═╩═") + "═╝");
 }
